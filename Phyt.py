c02c066dbdbc7415434f374677e73a852007e32e
Diff <tonkachiev152@gmail.com>
2026-07-20 18:49:36 +0500

    переменные

--- Phyt.py
+++ Phyt.py
@@ -1,2 +1,63 @@
-print("Hello world!")
-print("Egor QQ_ss_ss")
+#Strings
+
+First_name = "Bro"
+food = "pizza"
+Email = "bro@gmail.com"
+
+print(f"Hello {First_name}")
+print(f"my friend u like {food}?")
+print(f"and your Email it's {Email}?")
+
+#Integers
+
+age = 25
+quantuty = 3
+num_of_students = 30
+
+print(f"You are {age} year old")
+print(f"you are buying {quantuty} items")
+print(f"Your class has {num_of_students} students")
+
+#Float
+
+price = 19.99
+gpa = 5.5
+distance = 4.2
+
+print(f"The price is ${price}")
+print(f"Your gpa is: {gpa}")    
+print(f"you ran {distance}Km")
+
+#coolean
+
+is_student = False
+for_sale = True
+
+print(f"Are you a student?: {is_student}")
+
+
+if is_student:
+        print("you are a student")
+else:
+        print("You are NOT a student")
+
+if for_sale:
+        print("That item is for sale")
+else:
+        print("That item is Not avalible")
+
+#My comment
+
+user_name = "Diffman"
+Year = 2026
+Pi = 3.14
+Is_admin = True
+
+print(f"my name {user_name}")
+print(f"Now {Year}")
+print(f"Pi it's {Pi}")
+print("I am admin?")
+if Is_admin:
+    print("Yes")
+else:
+    print("Not")
