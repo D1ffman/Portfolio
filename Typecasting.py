5efb6b0c8ee9ca13ca80ebd9008658078866f1ca
Diff <tonkachiev152@gmail.com>
2026-07-21 15:38:27 +0500

    Typecasting

---
+++ Typecasting.py
@@ -0,0 +1,15 @@
+#Typecasting 
+
+name = "bro"
+age = 25
+gpa = 3.3
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+gpa = int(gpa)
+print(gpa)
+
