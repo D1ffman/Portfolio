32cd6bbd66cc2b562b2e5743745a1706be1c395d
Diff <tonkachiev152@gmail.com>
2026-07-25 17:59:14 +0500

    Игра Слов

---
+++ madlibs_game.py
@@ -0,0 +1,16 @@
+#Madlibs game 
+#Введи сначала слова, а потом увидишь результат )
+
+
+
+adjective1 = input("описание зоопрака: ")
+noun1 = input("введи кого ты там видел: ")
+adjective2 = input("прилагательное для этого существа: ")
+verb1 = input("введи что делало это существо: ")
+adjective3 = input("введи какое у тебя было натроение: ")
+
+
+print(f"Сегодня я пошел в {adjective1} зоопарк")
+print(f"на выставке я увидел {noun1}")
+print(f"{noun1} было {adjective2} и {verb1}")
+print(F"Я был {adjective3}!")
