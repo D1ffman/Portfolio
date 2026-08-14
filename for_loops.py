7a12ce8b9db9173ef4d77536be0baddff9194d82
Diff <tonkachiev152@gmail.com>
2026-08-14 14:48:28 +0500

    For loops add

---
+++ for_loops.py
@@ -0,0 +1,18 @@
+#Можно использовать с циклом строки, числа, диапазон, последовательности
+
+
+
+
+#for x in range(1, 15):
+#    print(x)
+
+#credit_card = "213123-123123-123"
+#for x in credit_card:
+#    print(x)
+
+for x in range(1, 21):
+    if x == 5:
+        break
+    else:
+        print(x) 
+
