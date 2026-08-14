f93c09086f1c98bf032c70b0c7905ab3d4750663
Diff <tonkachiev152@gmail.com>
2026-08-14 16:00:16 +0500

    Словарь - Dictionary

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+capitals = {"usa": "Washington",
+            "india": "New delhi",
+            "Russia": "Moscow"}
+
+#print(capitals.get("Russia")) - Найдет из списка элемент
+
+#if capitals.get ("Russia"):
+ #   print("Have")
+#else:
+ #   print("not have")
+
+#capitals.update({"Germany": "Berlin"})
+
+#keys = capitals.keys()           Покажет ключи 
+
+#for key in capitals.keys():
+    #print(key)
+
+#value  = capitals.values()       Значения в словоре выводит
+
+#for value in capitals.values():
+    #print(value)
+
+#items = capitals.items()             Выведет ключи  и значения столбиком
+
+#for key, value in capitals.items():
+    #print(f"{key}: {value}")
