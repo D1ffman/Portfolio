a3b52e1e9e639d35f04c96b4d5c62519e33d3452
Diff <tonkachiev152@gmail.com>
2026-08-20 18:32:38 +0500

    "Моудли и импорт"

---
+++ modules.py
@@ -0,0 +1,22 @@
+# import math
+# import math as m
+#from math import e
+
+#a, b ,c, d, e = 1, 2, 3, 4, 5
+
+#print(e ** a)
+#print(e ** b)
+#print(e ** c)
+#print(e ** d)
+#print(e ** e)
+
+
+import test
+
+result = test.pi
+result = test.square(3)
+result = test.cube(3)
+result = test.circumference(3)
+result = test.area(3)
+
+print(result)
