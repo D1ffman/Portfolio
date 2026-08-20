40791f9f1216c232aaa56452558c00fb0855b3b9
Diff <tonkachiev152@gmail.com>
2026-08-20 19:10:52 +0500

    "Классы и объекты, а также модель для опп"

---
+++ OOP_Classes_And_Objects.py
@@ -0,0 +1,11 @@
+from CARR import car
+
+car1 = car("Toyota", 2024, "red", False)
+car1.drive()
+
+
+print(car1.model)
+print("year my car", car1.year)
+print("color my car its", car1.color)
+print(car1.for_sale)
+
